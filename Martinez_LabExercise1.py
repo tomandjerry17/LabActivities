2abbd26eaa0654a3b00267bcb8fa8d25fca8c29f
tomandjerry17 <144357285+tomandjerry17@users.noreply.github.com>
2023-09-14 05:07:47 -0700

    Add files via upload

---
+++ Martinez_LabExercise1.py
@@ -0,0 +1,7 @@
+print("Thomas Gabriel D. Martinez")
+print("Bachelor of Science in Computer Science")
+print("CS1D")
+print("21 Years old")
+print("November 17, 2001")
+print("P1 Tidman Hinatuan Surigao Del Sur")
+
